6b1eedfc1eacc25e9f3f4e551acced0682808d8f
Michael Hall <mbhall88@gmail.com>
2018-04-09 11:13:10 +0100

    add test for read extraction multiple file types.

--- tests/test_bam2fast5.py
+++ tests/test_bam2fast5.py
@@ -112,6 +112,68 @@ class TestFastqReadIdExtraction(unittest.TestCase):
         self.assertSetEqual(result, expected)
 
 
+class TestReadIdExtraction(unittest.TestCase):
+    """Test the read id extracxtion functions for fastq, bam and sam"""
+
+    def test_ReadIdExtractionWithFast5_EmptySet(self):
+        """Test read id extracxtion from gzipped fastq with 6 mapped reads"""
+        mapped = False
+        files = ['tests/data/fast5/tb1.fast5']
+        result = bam2fast5.extract_read_ids(files, mapped)
+        expected = set()
+        self.assertSetEqual(result, expected)
+
+    def test_ReadIdExtractionWithFast5Fastq_TwoIds(self):
+        """Test read id extracxtion from gzipped fastq with 6 mapped reads"""
+        mapped = False
+        files = [
+            'tests/data/fast5/tb1.fast5',
+            'tests/data/fastq/ecoli_mapped.fastq.gz'
+        ]
+        result = bam2fast5.extract_read_ids(files, mapped)
+        expected = {
+            '6cf511b6-1724-46bd-b5a4-59c18bb57343',
+            '6c26d9b5-d892-4fc6-b035-abe575895c88'
+        }
+        self.assertSetEqual(result, expected)
+
+    def test_ReadIdExtractionWithFast5FastqBam_TwoIds(self):
+        """Test read id extracxtion from gzipped fastq with 6 mapped reads"""
+        mapped = True
+        files = [
+            'tests/data/fast5/tb1.fast5',
+            'tests/data/fastq/ecoli_mapped.fastq.gz',
+            'tests/data/bam/ecoli.bam'
+        ]
+        result = bam2fast5.extract_read_ids(files, mapped)
+        expected = {
+            '6cf511b6-1724-46bd-b5a4-59c18bb57343',
+            '6c26d9b5-d892-4fc6-b035-abe575895c88'
+        }
+        self.assertSetEqual(result, expected)
+
+    def test_ReadIdExtractionWithFast5FastqBam_EightIds(self):
+        """Test read id extracxtion from gzipped fastq with 6 mapped reads"""
+        mapped = True
+        files = [
+            'tests/data/fast5/tb1.fast5',
+            'tests/data/fastq/ecoli_mapped.fastq.gz',
+            'tests/data/bam/tb.bam'
+        ]
+        result = bam2fast5.extract_read_ids(files, mapped)
+        expected = {
+            '57d4cd63-3189-4006-93ec-bf3c8bfb2ce1',
+            'bbd563e9-1bf8-4268-92d5-45ccb8e3da72',
+            '8aecf428-af00-4791-b065-5d4abd798a29',
+            'd707ff64-6ade-477a-8b68-0b3c394ef3b1',
+            'c967d421-3da4-4e11-accf-0d3fb0155840',
+            '28acaa47-1cec-4a91-9abf-c780d27e6cc4',
+            '6cf511b6-1724-46bd-b5a4-59c18bb57343',
+            '6c26d9b5-d892-4fc6-b035-abe575895c88'
+        }
+        self.assertSetEqual(result, expected)
+
+
 class TestFastqRunIdExtraction(unittest.TestCase):
     """Test the read id extracxtion functions for fastq"""
 
